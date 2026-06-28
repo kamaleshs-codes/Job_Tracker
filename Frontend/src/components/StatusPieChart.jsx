53073c05dda19bf1a207b1b446f68e3c0b925499
kamaleshs-codes <skamalesh0204@outlook.com>
2026-06-28 23:35:14 +0530

    Built analytics with graphical represent to analyse detailed overview with job applications

---
+++ Frontend/src/components/StatusPieChart.jsx
@@ -0,0 +1,52 @@
+import {
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+const COLORS = [
+  "#3B82F6", // Pending - Blue
+  "#F59E0B", // Interview - Yellow
+  "#10B981", // Offer - Green
+  "#EF4444", // Rejected - Red
+];
+
+const StatusPieChart = ({ data }) => {
+  return (
+    <div className="w-full h-96">
+      <h2 className="text-2xl font-semibold mb-6">
+        Applications by Status
+      </h2>
+
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart>
+          <Pie
+            data={data}
+            dataKey="value"
+            nameKey="name"
+            cx="50%"
+            cy="50%"
+            outerRadius={120}
+            label
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={index}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+
+          <Tooltip />
+
+          <Legend />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default StatusPieChart;
--- Frontend/src/pages/Analytics.jsx
+++ Frontend/src/pages/Analytics.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import DashboardLayout from "../layouts/DashboardLayout";
+import api from "../api/axios";
+import StatusPieChart from "../components/StatusPieChart";
+import { FaExchangeAlt } from "react-icons/fa";
 
-function Analytics() {
+const Analytics = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    fetchJobs()
+  },[]);
+
+  const fetchJobs = async () => {
+    try {
+      const res = await api.get("/jobs");
+      setJobs(res.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+
+  // Analytics Data
+  const statusData = [
+    {
+      name: "Pending",
+      value: jobs.filter((job) => job.status === "Pending").length,
+    },
+    {
+      name: "Interview",
+      value: jobs.filter((job) => job.status === "Interview").length,
+    },
+    {
+      name: "Offer",
+      value: jobs.filter((job) => job.status === "Offer").length,
+    },
+    {
+      name: "Rejected",
+      value: jobs.filter((job) => job.status === "Rejected").length,
+    },
+  ];
+
+
+
+
   return (
-    <div>This is Analytics page</div>
-  )
-}
+    <DashboardLayout>
+      <div className='space-y-8'>
+        <div>
+          <h1 className='text-3xl font-bold text-gray-800'>
+            Analytics Dashboard
+          </h1>
 
-export default Analytics
+          <p className='text-gray-500 mt-2'>
+            Visualize your job application progress.
+          </p>
+        </div>
+
+        <div className='bg-white rounded-xl shadow-md p-6'>
+          <StatusPieChart data={statusData} />
+        </div>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default Analytics;
